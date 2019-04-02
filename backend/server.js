a81dba30f84a3e3bab9ee9a0c533448c26896c35
Zarnerz <holstein30@live.com>
2019-04-02 18:37:17 -0400

    Backend Server & DB connected

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
-const mongoose = require(“mongoose”);
-const express = require(“express”);
-var cors = require(‘cors’);
-const bodyParser = require(“body-parser”);
-const logger = require(“morgan”);
-const Data = require(“./data”);
+const mongoose = require("mongoose");
+const express = require("express");
+var cors = require("cors");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const Data = require("./data");
 
 const API_PORT = 3001;
 const app = express();
@@ -11,13 +11,11 @@ app.use(cors());
 const router = express.Router();
 
 // this is our MongoDB database
-const dbRoute = "mongodb://jelo:a9bc839993@ds151382.mlab.com:51382/jelotest";
+const dbRoute =
+  "mongodb://Main:poppylop0@ds253094.mlab.com:53094/mern-stack-app";
 
 // connects our back end code with the database
-mongoose.connect(
-  dbRoute,
-  { useNewUrlParser: true }
-);
+mongoose.connect(dbRoute, { useNewUrlParser: true });
 
 let db = mongoose.connection;
 
